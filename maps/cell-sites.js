a2f0f2cd2c362e99ee9d166cfc0e430afc10fd64
jsdnxx <jason@suarez.email>
2017-09-26 14:39:40 -0700

    script

--- maps/cell-sites.js
+++ maps/cell-sites.js
@@ -1,5 +1,21 @@
 const fs = require('fs')
-const base = fs.readFileSync('./municipios.geojson').toString()
+const csv = require('binary-csv')
+const base = JSON.parse(fs.readFileSync('./municipios.geojson').toString())
+const data = fs.createReadStream('./cell-sites-9-25-2017.csv')
 
-fs.writeFileSync('./cell-sites.geojson', new Buffer(base))
-console.log('done')
+"".in
+
+data.pipe(csv({json: true}))
+  .on('data', row => {
+    const feature = base.features
+      .filter(f => f.properties.name)
+      .filter(f => f.properties.name.localeCompare(row.County  + ' Municipio, PR', 'es', {sensitivity: 'base'}) == 0)
+      [0]
+    console.log(row.County, feature.properties)
+    Object.assign(feature.properties, row)
+  })
+  .on('end', () => {
+
+    fs.writeFileSync('./cell-sites.geojson', new Buffer(JSON.stringify(base)))
+    console.log('done')
+  })
